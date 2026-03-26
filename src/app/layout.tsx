cf46f75624072307c9390b33ecb6b3a598ef19f2
Felipe Salviano <felipesalviano00@hotmail.com>
2026-03-26 12:22:05 -0300

    Ajuste no icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ export const metadata: Metadata = {
   title: "Bonvechio Madeiras Tratadas",
   description:
     "Soluções em madeiras tratadas com qualidade, resistência e confiança para o seu projeto.",
+    icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
